89b89d0821c6454ac602d3f19ad2f2e9deeeeaf6
pongkitkkpk <pongkitkim@gmail.com>
2023-09-22 15:29:52 +0700

    file router3

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Router2 from "./router2";
+
+import Router3 from "./router3";
 
 
 function App() {
-  
+
   return (
-    <>
-      
-      <Router2/>
-    </>
+
+    <Router3/> 
+
   )
 }
 
