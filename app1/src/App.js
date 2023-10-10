154a3960b6c2217c071af56cc05b5b6dc9c9c498
pongkitkkpk <pongkitkim@gmail.com>
2023-10-10 19:01:06 +0700

    formvalidation

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-import FormCheck from "./form-boots/form-check";
+// import FormCheck from "./form-boots/form-check";
+import FormValidation from "./form-validation";
 
 export default function App() {
 
   return (
 
-    <FormCheck/>
+    <FormValidation/>
   )
 }
